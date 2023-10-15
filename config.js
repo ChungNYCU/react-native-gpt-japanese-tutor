4044e5ff1c86694c42ab3c07d5d631f3b2264972
Wei-Che Chung <wchung@seattleu.edu>
2023-10-14 23:52:21 -0700

    move consts into config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const AppConfig = {
+  // constants for override OpenAI's attributes
+  BASE_URL: 'https://api.openai.com/v1',
+
+  // constants for generate request body
+  MODEL: 'gpt-3.5-turbo-instruct',
+  MAX_TOKENS: 2000,
+  TEMPERATURE: 0,
+}
+
+export default AppConfig
