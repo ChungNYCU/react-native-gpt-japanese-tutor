e23204da6e7270e292ce9bc4b1cefaecb20efa9b
Wei-Che Chung <wchung@seattleu.edu>
2023-10-14 23:54:27 -0700

    move styles to separate files from listed screens

---
+++ src/styles/DictionaryScreenStyle.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native'
+
+const dictionaryScreenStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  input: {
+    width: '70%',
+    marginHorizontal: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+  },
+  resultContainer: {
+    flex: 10,
+  },
+  apiResult: {
+    marginTop: 10,
+    // fontFamily: 'monospace',
+  },
+})
+
+export default dictionaryScreenStyles
---
+++ src/styles/HomeScreenStyle.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+
+const HomeScreenStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
+
+export default HomeScreenStyle
