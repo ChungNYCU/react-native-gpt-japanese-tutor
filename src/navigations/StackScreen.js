7f323563f6f5dd0a9057598f852fbe1013b5d72d
Wei-Che Chung <wchung@seattleu.edu>
2023-10-17 13:40:37 -0700

    Replace tab navigator with stack navigator

---
+++ src/navigations/StackScreen.js
@@ -0,0 +1,47 @@
+import { View } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Ionicons } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native';
+
+import Button from '../components/Button'
+import HomeScreen from '../screens/HomeScreen'
+import DictionaryScreen from '../screens/DictionaryScreen'
+import SettingsScreen from '../screens/SettingScreen'
+import StackScreenStyle from '../styles/StackScreenStyle';
+
+const StackScreen = ({ routeName }) => {
+
+  const styles = StackScreenStyle
+  const Stack = createNativeStackNavigator()
+  const navigation = useNavigation();
+
+  const navigateToSettingScreen = () => {
+    navigation.navigate('Setting')
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Stack.Navigator
+        initialRouteName={routeName}
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Dictionary" component={DictionaryScreen} />
+        <Stack.Screen name="Setting" component={SettingsScreen} />
+      </Stack.Navigator>
+
+      <View style={styles.settingButtomContainer}>
+        <Button
+          onPress={navigateToSettingScreen}
+        >
+          <Ionicons name="settings-sharp" size={24} color="black" />
+        </Button>
+      </View>
+
+    </View>
+  )
+}
+
+export default StackScreen
---
+++ src/styles/StackScreenStyle.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+const StackScreenStyle = StyleSheet.create({
+  settingButtomContainer: {
+    position: 'absolute',
+    top: 50,
+    right: 10,
+  },
+})
+
+export default StackScreenStyle
