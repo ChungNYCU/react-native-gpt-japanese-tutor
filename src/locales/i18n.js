4366d87a441c134792e4227d643d945e5867fd95
Wei-Che Chung <wchung@seattleu.edu>
2023-10-18 10:29:50 -0700

    Initial feature of change system language

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -1,17 +1,30 @@
 import { I18n } from 'i18n-js'
 import * as Localization from 'expo-localization'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import { enUS } from './en-us'
 import { zhTW } from './zh-tw'
 
-const locale = Localization.locale
+const localeKey = 'locale' // The key used to store the locale in AsyncStorage
+
+const getStoredLocale = async () => {
+  try {
+    const storedLocale = await AsyncStorage.getItem(localeKey)
+    return storedLocale || Localization.locale // Use the stored locale or the device's default locale
+  } catch (error) {
+    console.error('Error retrieving locale from AsyncStorage:', error)
+    return Localization.locale // Fallback to the device's default locale on error
+  }
+}
 
 const i18n = new I18n({
   'en-US': enUS,
   'zh-TW': zhTW,
 })
 
-i18n.defaultLocale = locale
-i18n.locale = 'zh-TW'
+getStoredLocale().then((locale) => {
+  i18n.defaultLocale = locale
+  i18n.locale = locale
+})
 
 export default i18n
--- src/screens/SettingScreen.js
+++ src/screens/SettingScreen.js
@@ -3,10 +3,11 @@ import {
   View,
   Text,
   Switch,
-  StyleSheet,
   ScrollView,
   Keyboard,
+  Dimensions
 } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import i18n from '../locales/i18n'
 import Button from '../components/Button'
@@ -16,16 +17,32 @@ const styles = SettingScreenStyle
 
 const SettingsScreen = () => {
   useEffect(() => {
+    async function fetchLocale() {
+      try {
+        const locale = await AsyncStorage.getItem('locale')
+        if (locale !== null) {
+          setSelectedLanguage(locale)
+        }
+      } catch (error) {
+        console.error('Error fetching locale from AsyncStorage:', error)
+      }
+    }
     Keyboard.dismiss()
+    fetchLocale()
   }, [])
 
   const [selectedLanguage, setSelectedLanguage] = useState('')
-
   const [notificationsEnabled, setNotificationsEnabled] = useState(true)
   const [darkModeEnabled, setDarkModeEnabled] = useState(false)
 
-  const onLanguageChange = (value) => {
-    setSelectedLanguage(value)
+  const onLanguageChange = async () => {
+    const locale = await AsyncStorage.getItem('locale')
+    if (locale === 'en-US') {
+      await AsyncStorage.setItem('locale', 'zh-TW')
+    } else {
+      await AsyncStorage.setItem('locale', 'en-US')
+    }
+    setSelectedLanguage(await AsyncStorage.getItem('locale'))
   }
 
   const toggleNotifications = () => {
@@ -41,8 +58,8 @@ const SettingsScreen = () => {
       <ScrollView>
         <View style={styles.settingRow}>
           <Text style={styles.settingLabel}>{i18n.t('systemLanguage')}</Text>
-          <Button>
-            <Text style={styles.settingLabel}>English</Text>
+          <Button onPress={onLanguageChange}>
+            <Text style={styles.settingLabel}>{selectedLanguage}</Text>
           </Button>
         </View>
 
