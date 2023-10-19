1c6c1218665d1595fddf7f8c32dbf11328f2f4e5
Wei-Che Chung <wchung@seattleu.edu>
2023-10-19 13:15:57 -0700

    Add a place holder on the top  of screen

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+
+const Header = () => {
+  return <View style={styles.container}></View>
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ffffff',
+    width: '100%',
+    height: 90,
+  },
+})
+
+export default Header
