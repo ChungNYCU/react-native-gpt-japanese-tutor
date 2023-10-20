9068b8a962bb7bcf8f0e7c953961122559dde8f6
Wei-Che Chung <wchung@seattleu.edu>
2023-10-20 14:32:10 -0700

    Refine user interface on vocabulary display

--- src/components/JapaneseVocabularyDetails.js
+++ src/components/JapaneseVocabularyDetails.js
@@ -7,12 +7,13 @@ const JapaneseVocabularyDetails = ({ vocabularyData }) => {
   const { basic, detail } = vocabularyData
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>Basic Information:</Text>
-      <Text>Vocabulary: {basic.vocabulary}</Text>
-      <Text>Type: {basic.type}</Text>
-      <Text>Pronunciation (Hiragana): {basic['pronunciation-hiragana']}</Text>
-      <Text>Pronunciation (Romaji): {basic['pronunciation-romaji']}</Text>
+      <Text style={styles.text}>Vocabulary: {basic.vocabulary}</Text>
+      <Text style={styles.text}>Type: {basic.type}</Text>
+      <Text style={styles.subTitle}>Pronunciation:</Text>
+      <Text style={styles.text}>{basic['pronunciation-hiragana']}</Text>
+      <Text style={styles.text}>{basic['pronunciation-romaji']}</Text>
 
       <Text style={styles.title}>Detail:</Text>
       <Text>
@@ -23,16 +24,13 @@ const JapaneseVocabularyDetails = ({ vocabularyData }) => {
         <Text style={styles.title}>Sample Sentences:</Text>
         {detail['sample-sentences'].map((item, index) => (
           <View key={index}>
-            <Text>Sentence: {item.sentence}</Text>
-            <Text>
-              Pronunciation (Hiragana): {item['pronunciation-hiragana']}
-            </Text>
-            <Text>Pronunciation (Romaji): {item['pronunciation-romaji']}</Text>
-            <Text>
-              Translation:{' '}
+            <Text style={styles.text}>{item.sentence}</Text>
+            <Text style={styles.text}>{item['pronunciation-hiragana']}</Text>
+            <Text style={styles.text}>{item['pronunciation-romaji']}</Text>
+            <Text style={styles.text}>
               {item.translation.replace('${language}', 'Your Language')}
             </Text>
-            <View style={{ height: 1, backgroundColor: 'gray' }} />
+            <View style={styles.boarderLine} />
           </View>
         ))}
       </View>
@@ -43,13 +41,12 @@ const JapaneseVocabularyDetails = ({ vocabularyData }) => {
         horizontal={true}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item }) => (
-          <View>
-            <Text>Form: {item.form}</Text>
-            <Text>Variation: {item.variation}</Text>
-            <Text>
-              Pronunciation (Hiragana): {item['pronunciation-hiragana']}
-            </Text>
-            <Text>Pronunciation (Romaji): {item['pronunciation-romaji']}</Text>
+          <View style={styles.tenseCard}>
+            <Text style={styles.text}>Form: {item.form}</Text>
+            <Text style={styles.text}>Variation: {item.variation}</Text>
+            <Text style={styles.subTitle}>Pronunciation:</Text>
+            <Text style={styles.text}>{item['pronunciation-hiragana']}</Text>
+            <Text style={styles.text}>{item['pronunciation-romaji']}</Text>
           </View>
         )}
       />
@@ -59,15 +56,28 @@ const JapaneseVocabularyDetails = ({ vocabularyData }) => {
 
 const styles = StyleSheet.create({
   container: {
+    marginHorizontal: '1%',
     backgroundColor: '#ffffff',
-    width: '100%',
-    height: 90,
+    width: '98%',
   },
   title: {
+    marginTop: 5,
+    marginBottom: 1,
     fontWeight: '900',
   },
   subTitle: {
+    marginTop: 3,
     fontWeight: '700',
+  },
+  text: {
+    marginVertical: 3,
+  },
+  boarderLine: {
+    height: 1,
+    backgroundColor: 'gray',
+  },
+  tenseCard: {
+    marginRight: 20,
   },
 })
 
