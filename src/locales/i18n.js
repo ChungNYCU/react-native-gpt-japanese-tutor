b0dba374c3fc6b4b28732198db8a335fbd5a24d8
Wei-Che Chung <wchung@seattleu.edu>
2023-10-20 14:09:21 -0700

    A file to handle strings, only load require lang

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -2,27 +2,36 @@ import { I18n } from 'i18n-js'
 import * as Localization from 'expo-localization'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-import { enUS } from './en-us'
-import { zhTW } from './zh-tw'
+import { locales } from './locales'
 
-const localeKey = 'locale' // The key used to store the locale in AsyncStorage
+const UI_LANGUAGE_KEY = locales.UI_LANGUAGE_KEY
+
+const i18n = new I18n()
 
 const getStoredLocale = async () => {
   try {
-    const storedLocale = await AsyncStorage.getItem(localeKey)
-    return storedLocale || Localization.locale // Use the stored locale or the device's default locale
+    const storedLocale = await AsyncStorage.getItem(UI_LANGUAGE_KEY)
+    return storedLocale || Localization.locale
   } catch (error) {
     console.error('Error retrieving locale from AsyncStorage:', error)
-    return Localization.locale // Fallback to the device's default locale on error
+    return Localization.locale
   }
 }
 
-const i18n = new I18n({
-  'en-US': enUS,
-  'zh-TW': zhTW,
-})
-
 getStoredLocale().then((locale) => {
+  switch (locale) {
+    case 'en-US':
+      const enUS = require('./en-us.json')
+      i18n.translations = { 'en-US': enUS }
+      break
+    case 'zh-TW':
+      const zhTW = require('./zh-tw.json')
+      i18n.translations = { 'zh-TW': zhTW }
+      break
+    default:
+      const en = require('./en-us.json')
+      i18n.translations = { 'en-US': en }
+  }
   i18n.defaultLocale = locale
   i18n.locale = locale
 })
---
+++ src/locales/locales.js
@@ -0,0 +1,13 @@
+export const locales = {
+    UI_LANGUAGE_KEY: 'UI_LANGUAGE_KEY',
+    systemLanguage: "systemLanguage",
+    enableNotifications: "enableNotifications",
+    darkMode: "darkMode",
+    settings: "settings",
+    inputPlaceHolder: "inputPlaceHolder",
+    english: "english",
+    traditionalChinese: "traditionalChinese",
+    loading: "loading",
+    "zh-TW": "繁體中文",
+    "en-US": "English",
+};
