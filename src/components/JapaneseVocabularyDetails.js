0efcd622ab083eb1e1d8d6aca94d1528104d1e56
Wei-Che Chung <wchung@seattleu.edu>
2023-10-25 13:38:01 -0700

    Add error handling when invalid object passed in

--- src/components/JapaneseVocabularyDetails.js
+++ src/components/JapaneseVocabularyDetails.js
@@ -8,59 +8,66 @@ const JapaneseVocabularyDetails = ({ vocabularyData }) => {
 
   const { basic, detail } = vocabularyData
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Basic Information:</Text>
-      <Text style={styles.text}>Vocabulary: {basic.vocabulary}</Text>
-      <Text style={styles.text}>Type: {basic.type}</Text>
-      <Text style={styles.subTitle}>Pronunciation:</Text>
-      <Text style={styles.text}>{basic['pronunciation-hiragana']}</Text>
-      <Text style={styles.text}>{basic['pronunciation-romaji']}</Text>
+  try {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Basic Information:</Text>
+        <Text style={styles.text}>Vocabulary: {basic.vocabulary}</Text>
+        <Text style={styles.text}>Type: {basic.type}</Text>
+        <Text style={styles.subTitle}>Pronunciation:</Text>
+        <Text style={styles.text}>{basic['pronunciation-hiragana']}</Text>
+        <Text style={styles.text}>{basic['pronunciation-romaji']}</Text>
 
-      <Text style={styles.title}>Detail:</Text>
-      <Text>
-        Definition: {detail.definition.replace('${language}', 'Your Language')}
-      </Text>
+        <Text style={styles.title}>Detail:</Text>
+        <Text>
+          Definition:{' '}
+          {detail.definition.replace('${language}', 'Your Language')}
+        </Text>
 
-      <View>
-        <Text style={styles.title}>Sample Sentences:</Text>
-        {detail['sample-sentences'].map((item, index) => (
-          <View key={index}>
-            <Text style={styles.text}>{item.sentence}</Text>
-            <Text style={styles.text}>{item['pronunciation-hiragana']}</Text>
-            <Text style={styles.text}>{item['pronunciation-romaji']}</Text>
-            <Text style={styles.text}>
-              {item.translation.replace('${language}', 'Your Language')}
-            </Text>
-            <View style={styles.boarderLine} />
-          </View>
-        ))}
+        <View>
+          <Text style={styles.title}>Sample Sentences:</Text>
+          {detail['sample-sentences'].map((item, index) => (
+            <View key={index}>
+              <Text style={styles.text}>{item.sentence}</Text>
+              <Text style={styles.text}>{item['pronunciation-hiragana']}</Text>
+              <Text style={styles.text}>{item['pronunciation-romaji']}</Text>
+              <Text style={styles.text}>
+                {item.translation.replace('${language}', 'Your Language')}
+              </Text>
+              <View style={styles.boarderLine} />
+            </View>
+          ))}
+        </View>
+
+        <Text style={styles.title}>Grammatical Tenses:</Text>
+        <FlatList
+          data={detail['grammatical-tenses']}
+          horizontal={true}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.tenseCard}>
+              <Text style={styles.text}>Form: {item.form}</Text>
+              <Text style={styles.text}>Variation: {item.variation}</Text>
+              <Text style={styles.subTitle}>Pronunciation:</Text>
+              <Text style={styles.text}>{item['pronunciation-hiragana']}</Text>
+              <Text style={styles.text}>{item['pronunciation-romaji']}</Text>
+            </View>
+          )}
+        />
       </View>
-
-      <Text style={styles.title}>Grammatical Tenses:</Text>
-      <FlatList
-        data={detail['grammatical-tenses']}
-        horizontal={true}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.tenseCard}>
-            <Text style={styles.text}>Form: {item.form}</Text>
-            <Text style={styles.text}>Variation: {item.variation}</Text>
-            <Text style={styles.subTitle}>Pronunciation:</Text>
-            <Text style={styles.text}>{item['pronunciation-hiragana']}</Text>
-            <Text style={styles.text}>{item['pronunciation-romaji']}</Text>
-          </View>
-        )}
-      />
-    </View>
-  )
+    )
+  } catch (error) {
+    return <Text>error</Text>
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     marginHorizontal: '1%',
-    backgroundColor: '#ffffff',
+    backgroundColor: '#FFF59E',
     width: '98%',
+    padding: 10,
+    borderRadius: 10,
   },
   title: {
     marginTop: 5,
